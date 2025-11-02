7a61d3866ffcf08767e5d285b61aea76387be953
Germano Corrêa <germanobruscato@gmail.com>
2025-11-02 17:26:53 -0300

    fix: melhora finalizacao do programa, e codigos de teste adicionados

--- computador-t2.hs
+++ computador-t2.hs
@@ -1,3 +1,5 @@
+import Debug.Trace
+
 type Address = Int
 
 type Content = Int
@@ -26,10 +28,13 @@ data ControlSignals = ControlSignals
   { read_from_mem :: Signal,
     write_to_mem :: Signal,
     write_back_to_acc :: Signal,
-    write_back_to_pc :: Signal
+    write_back_to_pc :: Signal,
+    finish :: Signal
   }
+  deriving (Show)
 
-data State = State {mem :: Mem, pc :: IC, ireg :: IReg, acc :: ACC, eqz :: EQZ, rdm :: RDM, regMemAdd :: REM, control :: ControlSignals}
+-- rem como rem' pq rem já existe no prelude
+data State = State {mem :: Mem, pc :: IC, ireg :: IReg, acc :: ACC, eqz :: EQZ, rdm :: RDM, rem' :: REM, control :: ControlSignals} deriving (Show)
 
 lod = 2
 
@@ -49,38 +54,77 @@ nop = 18
 
 hcf = 20
 
+programaCarregado :: Mem
+programaCarregado =
+  [ -- Código do programa (iniciando em 0)
+    (0, 2), -- Instrução LOD [cite: 53]
+    (1, 240), -- Operando <end> (Endereço de A)
+    (2, 14), -- Instrução ADD [cite: 53]
+    (3, 241), -- Operando <end> (Endereço de B)
+    (4, 4), -- Instrução STO [cite: 53]
+    (5, 251), -- Operando <end> (Endereço de Resp)
+    (6, 20), -- Instrução HCF [cite: 53]
+    (7, 18), -- Operando NOP (conforme exemplo do PDF) [cite: 80]
+
+    -- Dados do programa [cite: 7]
+    (240, 10), -- Valor inicial de A
+    (241, 5), -- Valor inicial de B
+    (251, 0) -- Valor inicial de Resp (será sobrescrito)
+  ]
+
+initialState :: State
+initialState =
+  State
+    { mem = [],
+      pc = 0, -- O contador de instruções é inicializado com zero [cite: 45]
+      ireg = (0, 0),
+      acc = 0,
+      eqz = True, -- EQZ indica se o acumulador é igual a zero [cite: 48]
+      rdm = 0,
+      rem' = 0,
+      control =
+        ControlSignals
+          { read_from_mem = False,
+            write_to_mem = False,
+            write_back_to_acc = False,
+            write_back_to_pc = False,
+            finish = False
+          }
+    }
+
 -- FETCH, DECODE, EXECUTE, MEMORY ACCESS, WRITE BACK
 -- Memory antes de execute????
 cpu :: State -> State
 cpu state = finalState
   where
-    state1 = fetch state
+    cleanState = state {ireg = (0, 0), eqz = 0 == acc state, rdm = 0, rem' = 0, control = (control state) {read_from_mem = False, write_to_mem = False, write_back_to_acc = False, write_back_to_pc = False, finish = False}}
+    state1 = fetch cleanState
     state2 = decode state1
-    state3 = state2 {regMemAdd = snd (ireg state2), rdm = acc state2}
+    state3 = state2 {rem' = snd (ireg state2), rdm = acc state2}
     state4 = memory state3
     state5 = execute state4
     state6 = writeBack state5
-    isHalt = fst (ireg state) == hcf
-    state7 = state6 {ireg = (0, 0), eqz = 0 == acc state6, rdm = 0, regMemAdd = 0, control = (control state6) {read_from_mem = False, write_to_mem = False, write_back_to_acc = False, write_back_to_pc = False}}
-    finalState = if isHalt then state7 else cpu state7
+    finalState = if finish (control state6) then state6 else cpu state6
 
 fetch :: State -> State
 fetch state = finalState
   where
-    state1 = state {regMemAdd = pc state} -- salva PC no registrador REM
+    state1 = state {rem' = pc state} -- salva PC no registrador REM
     state2 = memoryRead state1 -- Le REM da memoria e salva em RDM
     state3 = state2 {ireg = (rdm state2, 0)} -- Salva RDM na posicao alta de IReg
-    state4 = state3 {regMemAdd = pc state3 + 1} -- incrementa pc para a prox linha da memoria, o parametro da instrucao
+    state4 = state3 {rem' = pc state3 + 1} -- incrementa pc para a prox linha da memoria, o parametro da instrucao
     state5 = memoryRead state4 -- Le REM da memoria e salva em RDM
     state6 = state5 {ireg = (fst (ireg state5), rdm state5)} -- Salva RDM na posicao baixa de IReg
-    finalState = state6 {regMemAdd = pc state6 + 1} -- incrementa PC para o proximo ciclo
+    finalState = state6 {pc = pc state6 + 2} -- incrementa PC para o proximo ciclo
 
 decode :: State -> State
 decode state
   | ins == sto = state {control = (control state) {write_to_mem = True}}
-  | ins == lod || ins == cpe || ins == add || ins == sub = state {control = (control state) {write_back_to_acc = True, read_from_mem = True}}
+  | ins == cpe || ins == add || ins == sub = state {control = (control state) {read_from_mem = True}} -- apesar de escreverem em ACC, a ULA faz isso direto pois ela possui conexao
+  | ins == lod = state {control = (control state) {write_back_to_acc = True, read_from_mem = True}}
   | ins == jmp = state {control = (control state) {write_back_to_pc = True}}
   | ins == jmz = state {control = (control state) {write_back_to_pc = eqzFlag}}
+  | ins == hcf = state {control = (control state) {finish = True}}
   | otherwise = state
   where
     ins = fst (ireg state)
@@ -104,14 +148,15 @@ execute state
 writeBack :: State -> State
 writeBack state
   | write_back_to_pc (control state) = state {pc = snd (ireg state)}
+  | write_back_to_acc (control state) = state {acc = rdm state}
   | otherwise = state
 
 {-- CONTROLADOR DE MEMORIA --}
 memoryRead :: State -> State
-memoryRead state = state {rdm = selectAddress (regMemAdd state) (mem state)}
+memoryRead state = state {rdm = selectAddress (rem' state) (mem state)}
 
 memoryWrite :: State -> State
-memoryWrite state = state {mem = persistAddress (regMemAdd state) (rdm state) (mem state)}
+memoryWrite state = state {mem = persistAddress (rem' state) (rdm state) (mem state)}
 
 selectAddress :: Address -> Mem -> Content
 selectAddress _ [] = 0
@@ -120,7 +165,7 @@ selectAddress address (item : memory)
   | otherwise = selectAddress address memory
 
 persistAddress :: Address -> Content -> Mem -> Mem
-persistAddress _ _ [] = []
+persistAddress address content [] = [(address, content)]
 persistAddress address content (item : memory)
   | fst item == address = (address, content) : memory
   | otherwise = item : persistAddress address content memory
